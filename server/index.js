0c8369945da7be52bde2aaecc0945557469c1085
dnirns <daniel.irons@gmail.com>
2021-04-27 12:37:56 +0100

    new heroku test build

--- server/index.js
+++ server/index.js
@@ -9,3 +9,12 @@ const app = express()
 applyMiddleware(app)
 composeRoutes(app)
 app.listen(process.env.REACT_APP_SERVER_PORT)
+
+
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('./build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
