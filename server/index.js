bd88763555580f1465cfa541411d75a3f7694495
dnirns <daniel.irons@gmail.com>
2021-04-27 13:23:10 +0100

    new heroku test build 2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 require('dotenv').config()
 
 const express = require('express')
@@ -8,4 +10,13 @@ const app = express()
 
 applyMiddleware(app)
 composeRoutes(app)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  )
+}
+
 app.listen(process.env.REACT_APP_SERVER_PORT)
