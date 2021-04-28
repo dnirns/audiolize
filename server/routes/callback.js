6e91bb539ba454cef30a74873d867d75dcabc1b9
dnirns <daniel.irons@gmail.com>
2021-04-28 11:54:56 +0100

    fixed project root wrong redirect variable for callback

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -38,7 +38,7 @@ module.exports = (app) => {
       })
       res.cookie(process.env.REACT_APP_REFRESH_CODE, code, { expires: 0 })
       if (process.env.REACT_APP_NODE_ENV === 'production') {
-        res.redirect(process.env.REACT_APP_PROJECT_ROOT + '/visualizer')
+        res.redirect(process.env.REACT_APP_PROJECT_ROOT + 'visualizer')
       } else {
         res.redirect('http://localhost:3000/visualizer')
       }
