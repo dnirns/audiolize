ff7b3d884bc3fea8235b5b30de889a4a95feef4d
dnirns <daniel.irons@gmail.com>
2021-04-28 19:33:57 +0100

    icon to home link

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom'
 import Spotify_Icon from '../../assets/Spotify_Icon_RGB_Green.png'
 
 const Icon = ({ pulse }) => {
   return (
-    <a href='https://www.spotify.com'>
+    <Link to='/'>
       <img
         className={`${pulse && 'icon-pulse'}  spotify-icon`}
         src={Spotify_Icon}
         alt='spotify icon'
       />
-    </a>
+    </Link>
   )
 }
 
