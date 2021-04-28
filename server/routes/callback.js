4e24d6b0d7b57b7b1d33c3a7573141a7fe5d5160
dnirns <daniel.irons@gmail.com>
2021-04-28 14:28:56 +0100

    redirect callback to root url

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -38,7 +38,8 @@ module.exports = (app) => {
       })
       res.cookie(process.env.REACT_APP_REFRESH_CODE, code, { expires: 0 })
       if (process.env.REACT_APP_NODE_ENV === 'production') {
-        res.redirect(process.env.REACT_APP_PROJECT_ROOT)
+        // res.redirect(process.env.REACT_APP_PROJECT_ROOT)
+        res.redirect('https://https://spotify-visualz.herokuapp.com')
       } else {
         res.redirect('http://localhost:3000')
       }
